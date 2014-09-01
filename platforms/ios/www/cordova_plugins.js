56fe00545c165e2f82d57a7da2b8b4cbeca5a773
Tom Gersic <tgersic@salesforce.com>
2014-08-31 22:22:27 -0500

    loading app.js

--- platforms/ios/www/cordova_plugins.js
+++ platforms/ios/www/cordova_plugins.js
@@ -40,6 +40,20 @@ module.exports = [
         "id": "com.salesforce.util.push"
     },
     {
+        "file": "plugins/org.apache.cordova.console/www/console-via-logger.js",
+        "id": "org.apache.cordova.console.console",
+        "clobbers": [
+            "console"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.console/www/logger.js",
+        "id": "org.apache.cordova.console.logger",
+        "clobbers": [
+            "cordova.logger"
+        ]
+    },
+    {
         "file": "plugins/org.apache.cordova.device/www/device.js",
         "id": "org.apache.cordova.device.device",
         "clobbers": [
@@ -58,6 +72,7 @@ module.exports.metadata =
 // TOP OF METADATA
 {
     "com.salesforce": "2.3.0",
+    "org.apache.cordova.console": "0.2.10",
     "org.apache.cordova.device": "0.2.11",
     "com.phonegap.plugins.PushPlugin": "2.2.1"
 }
--- platforms/ios/www/js/app.js
+++ platforms/ios/www/js/app.js
@@ -2,17 +2,6 @@
 
     "use strict";
 
-    /* Adding platform (ios/android) specific css */
-    var platformStyle = document.createElement('link');
-    platformStyle.setAttribute('rel', 'stylesheet');
-    if (/Android/.test(navigator.userAgent)) {
-        platformStyle.setAttribute('href', 'css/ratchet-theme-android.css');
-    } else if (/iPhone/.test(navigator.userAgent)) {
-        platformStyle.setAttribute('href', 'css/ratchet-theme-ios.css');
-    }
-    document.querySelector('head').appendChild(platformStyle);
-
-
     /* Wait until cordova is ready to initiate the use of cordova plugins and app launch */
     document.addEventListener("deviceready", function() {
         authenticateUser(showUsersList);
@@ -52,7 +41,8 @@
                 listItemsHtml += ('<li class="table-view-cell"><div class="media-body">' + users[i].Name + '</div></li>');
             }
 
-            document.querySelector('#users').innerHTML = listItemsHtml;
+            console.log(listItemsHtml);
+            //document.querySelector('#users').innerHTML = listItemsHtml;
         })
     }
 
