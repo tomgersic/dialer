6f36d70a90baf4ed6737ff20421a1a19647673d3
Tom Gersic <tgersic@salesforce.com>
2014-09-01 22:32:01 -0500

    cleanup and adding smartsync.js

--- platforms/ios/www/js/app.js
+++ platforms/ios/www/js/app.js
@@ -37,31 +37,5 @@
         }
     }
 
-    /* This method will render a list of users from current salesforce org */
-    var showUsersList = function(forceClient) {
-
-        fetchRecords(forceClient, function(data) {
-            var users = data.records;
-
-            //console.log(data.records);
-
-            var listItemsHtml = '';
-            for (var i=0; i < users.length; i++) {
-                listItemsHtml += ('<li class="table-view-cell"><div class="media-body">' + users[i].Name + '</div></li>');
-            }
-
-            //console.log(listItemsHtml);
-            //document.querySelector('#users').innerHTML = listItemsHtml;
-        })
-    }
-
-    /* This method will fetch a list of user records from salesforce. 
-    Just change the soql query to fetch another sobject. */
-    var fetchRecords = function (forceClient, successHandler) {
-        var soql = 'SELECT Id, Name FROM User LIMIT 10';
-        forceClient.query(soql, successHandler, function(error) {
-            alert('Failed to fetch users: ' + error);
-        });
-    };
 
 })();
