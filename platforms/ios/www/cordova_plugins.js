583c3456ead96ae9f00d42ba49b4e86c3be697f0
Tom Gersic <tgersic@salesforce.com>
2014-09-06 23:40:44 -0500

    added connection info plugin for cordova. Probably wasn't needed after all, but it's there now

--- platforms/ios/www/cordova_plugins.js
+++ platforms/ios/www/cordova_plugins.js
@@ -54,6 +54,21 @@ module.exports = [
         ]
     },
     {
+        "file": "plugins/org.apache.cordova.network-information/www/network.js",
+        "id": "org.apache.cordova.network-information.network",
+        "clobbers": [
+            "navigator.connection",
+            "navigator.network.connection"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.network-information/www/Connection.js",
+        "id": "org.apache.cordova.network-information.Connection",
+        "clobbers": [
+            "Connection"
+        ]
+    },
+    {
         "file": "plugins/org.apache.cordova.device/www/device.js",
         "id": "org.apache.cordova.device.device",
         "clobbers": [
@@ -73,6 +88,7 @@ module.exports.metadata =
 {
     "com.salesforce": "2.3.0",
     "org.apache.cordova.console": "0.2.10",
+    "org.apache.cordova.network-information": "0.2.12-dev",
     "org.apache.cordova.device": "0.2.11",
     "com.phonegap.plugins.PushPlugin": "2.2.1"
 }
